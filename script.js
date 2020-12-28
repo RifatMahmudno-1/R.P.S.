1cbe44ac3596881e32e5ada86c99e9eebd95a047
Rifat Mahmud <rifatmahmudpc@gmail.com>
2020-12-28 07:08:16 +0600

    me

--- script.js
+++ script.js
@@ -1,68 +1,80 @@
-var splayer = 0;
-var scomputer = 0;
-var psimg = document.querySelector(".psimg");
-var csimg = document.querySelector(".csimg");
+var player;
+var computer;
+var plscore = 0;
+var pcscore = 0;
+var psimg = document.querySelector('.psimg');
+var csimg = document.querySelector('.csimg');
+var Inp = document.querySelector(".inp");
+var rpc = document.querySelector('.rpcimg');
+rpc.style.display = 'none';
 document.querySelector(".btn").addEventListener("click", function () {
-	document.querySelector(".rpcimg").style = "display: grid;grid-template-columns: 30% 30% 30%; justify-content: space-around;";
-	document.querySelector(".btn").style.display = "none";
-	document.querySelector(".endpoint").style.display = "none";
+	if (Inp.value > 0 && Inp.value <= 100) {
+		document.querySelector(".rpcimg").style.display = 'grid';
+		document.querySelector(".btn").style.display = "none";
+		document.querySelector(".endpoint").style.display = "none";
+		document.querySelector(".h1al").style.display = 'none';
+	} else {
+		document.querySelector(".h1al").style.display = 'block';
+	}
 });
 
 document.querySelector(".btn-1").addEventListener("click", function () {
-	pIscore = 1;
-	main();
+	player = 1;
+	image(); main(); final(); input();
 });
 
 document.querySelector(".btn-2").addEventListener("click", function () {
-	pIscore = 2;
-	main();
+	player = 2;
+	image(); main(); final(); input();
+
 });
 
 document.querySelector(".btn-3").addEventListener("click", function () {
-	pIscore = 3;
-	main();
+	player = 3;
+	image(); main(); final(); input();
+
 });
 
-function main() {
-	var finish = document.querySelector(".inp").value;
-	cIscore = Math.floor(Math.random() * 3 + 1);
-	document.querySelector(".draw").style.display = "none";
-	psimg.src = "dice-" + pIscore + ".png";
-	csimg.src = "dice-" + cIscore + ".png";
-
-	if (splayer == finish || scomputer == finish) {
-		winner();
-	} else if (pIscore == 1 && cIscore == 3) {
-		splayer++;
-	} else if (pIscore == 2 && cIscore == 1) {
-		splayer++;
-	} else if (pIscore == 3 && cIscore == 2) {
-		splayer++;
-	} else if (cIscore == 1 && pIscore == 3) {
-		scomputer++;
-	} else if (cIscore == 2 && pIscore == 1) {
-		scomputer++;
-	} else if (cIscore == 3 && pIscore == 2) {
-		scomputer++;
-	} else {
-		document.querySelector(".draw").style = "display:block;text-align:center; font-size: 30px; color: blue";
-		document.querySelector(".draw").innerHTML = "<h1>Draw!!<h1>";
-	}
-	document.querySelector(".splayer").textContent = splayer;
-	document.querySelector(".scomputer").textContent = scomputer;
-	console.log(pIscore, cIscore, finish);
+function image() {
+	//var finish = document.querySelector(".inp").value;
+	computer = Math.floor(Math.random() * 3 + 1);
+	psimg.src = "dice-" + player + ".png";
+	csimg.src = "dice-" + computer + ".png";
+	document.querySelector('.draw').style.display = 'none';
 }
 
-function winner() {
-	var finish = document.querySelector(".inp").value;
-	if (splayer == finish && splayer > 0) {
-		document.querySelector(".winner").innerHTML = '<h1 style="text-align:center; font-size: 200%; color: green; margin:2% 0;">You Won<br><Button onclick="window.location.reload()" style="background: linear-gradient(rgb(255, 0, 119),rgb(255, 0, 179)); border: none; padding: 1% 2%; font-size: 60%">Restart</Button></h1>';
-		document.querySelector(".rpcimg").style.display = "none";
-	} else if (scomputer == finish && scomputer > 0) {
-		document.querySelector(".winner").innerHTML = '<h1 style="text-align:center; font-size: 200%; color: red; margin:2% 0;">You Lose<br><Button onclick="window.location.reload()" style="background: linear-gradient(rgb(255, 0, 119),rgb(255, 0, 179)); border: none; padding: 1% 2%; font-size: 60%">Restart</Button></h1>';
-		document.querySelector(".rpcimg").style.display = "none";
+function main() {
+	if (player == 1 && computer == 2) {
+		pcscore++;
+	} else if (player == 2 && computer == 1) {
+		plscore++;
+	} else if (player == 3 && computer == 2) {
+		plscore++;
+	} else if (player == 2 && computer == 3) {
+		pcscore++;
+	} else if (player == 1 && computer == 3) {
+		plscore++;
+	} else if (player == 3 && computer == 1) {
+		pcscore++;
 	} else {
-		document.querySelector(".winner").innerHTML = '<h1 style="text-align:center; font-size: 100%; color: black; margin:2% 0;">You forgot to set winning point<br><Button onclick="window.location.reload()" style="background: linear-gradient(rgb(255, 0, 119),rgb(255, 0, 179)); border: none; padding: 1% 2%; font-size: 100%">Restart Now</Button></h1>';
-		document.querySelector(".rpcimg").style.display = "none";
+		document.querySelector('.draw').style.display = 'block';
 	}
+}
+
+function final() {
+	if (plscore == Inp.value) {
+		document.querySelector('.win_pl').style.display = 'block'
+		rpc.style.display = 'none';
+	} else if (pcscore == Inp.value) {
+		document.querySelector('.win_pc').style.display = 'block'
+		rpc.style.display = 'none';
+	}
+};
+
+function input() {
+	document.querySelector('.splayer').textContent = plscore;
+	document.querySelector('.scomputer').textContent = pcscore;
+	if (player == computer) {
+		document.querySelector('.draw').style.display = 'block';
+	};
 }
